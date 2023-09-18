59b26ec59b39de18238f62dba86cef29f574b4f2
MuneLiesWithin <romulo_medeiros@hotmail.com>
2023-09-18 14:51:12 -0300

    second

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../authService.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() {}
+  async ngOnInit(): Promise<void> {
+    if (await this.authService.isAutheticated())
+      this.router.navigate(['login-page']);
 
+    console.log(this.authService.user);
+  }
 }
