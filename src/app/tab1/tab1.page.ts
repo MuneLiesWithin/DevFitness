6356316c7c75eb6cb46824e077a294420c5376c2
MuneLiesWithin <romulo_medeiros@hotmail.com>
2023-09-20 18:51:59 -0300

    update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Exercicio, Treino} from '../../model/estruturas'
 import { TreinoService } from '../services/treino.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -11,10 +12,11 @@ export class Tab1Page {
 
   treinos : Treino [] = []
 
-  constructor(private serv: TreinoService) {
+  constructor(private serv: TreinoService, private router: Router) {
     this.treinos = this.serv.treinos
   }
-  excluir(id : number) {
-    this.serv.deleteTreino(id)
+
+  navToExercicios(item: Treino){
+    this.router.navigate(["/exercicios/"+item.id])
   }
 }
